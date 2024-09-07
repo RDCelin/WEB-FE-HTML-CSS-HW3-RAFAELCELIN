5b675f91998e145fb9d6bf0f3f4e6b138dd7ba3a
RDCelin <92396800+RDCelin@users.noreply.github.com>
2024-09-06 20:28:34 -0500

    Add files via upload

---
+++ Async.js
@@ -0,0 +1,7 @@
+ 
+ //Async is a boolean attribute and its job is:
+ //when true the scripts is downloaded alongside the page and is executed as soon as possible
+ let age = 37
+ console.log("Age:", age)
+ alert("age is "+ age);
+ document.getElementById("age").innerHTML = age;
